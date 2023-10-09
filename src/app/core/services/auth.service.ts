6fa4ad8cc1751aef3355771f9c90989c79c8e642
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2023-10-09 14:34:31 +0200

    security: use gitleaks to check for secret leaks
    ref: #157

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -15,7 +15,7 @@ export class AuthService {
 
     // You could pass these parameters in your environment variables
     private readonly redirectUri = 'https://a.myho.st:8888/check';
-    private readonly clientId = 'W4JkWYy4Qy1PogYmwOBt9I3HhQlzqD2m';
+    private readonly clientId = 'W4JkWYy4Qy1PogYmwOBt9I3HhQlzqD2m'; // gitleaks:allow
     private readonly oauth2SdkUrl = 'https://auth.demo.eniblock.com';
 
     // Method to initiate login process
